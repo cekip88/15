02dd6607ccd79004b04377b3710a9364db0aa216
cekip88 <cekip@mail.ru>
2020-11-11 01:46:26 +0600

    init: реализованы методы:  1. Проверка возможности хода  2. Ход  3. Сохранение игры в localStorage  4. Загрузка игры  5. Проверка на победу

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ class Game {
 
 		_.init();
 		_.start = false;
+		_.size = 4;
+		_.positions = {};
 	}
 
 	// создает тэги
@@ -38,129 +40,256 @@ class Game {
 				_.createEl('INPUT','choose-input',{'text' : '4'})
 			]});
 		let button = _.createEl('BUTTON','btn first-screen-btn',{'text' : 'Начать игру'});
+
+		document.querySelector('body').append(title,sizeCount,button);
+
+		sizeCount.querySelector('input').addEventListener('input',function () {
+			_.startSizeInputCheck(this)
+		});
 		button.addEventListener('click',function () {
 			_.size = sizeCount.querySelector('input').value * 1;
 			if (_.size < 3) _.size = 3;
 			else if (_.size > 8) _.size = 8;
-			_.newGame(_.size)
+			_.newGame()
 		});
-		document.querySelector('body').append(title,sizeCount,button);
+
 	}
+	startSizeInputCheck(input){
+		if (isNaN(input.value[input.value.length - 1] * 1)) input.value = input.value.substr(0,input.value.length - 1);
+	}
+
+
+
 
 
 	// запускает постройку игровой страницы
-	newGame(size){
+	newGame(){
 		const _ = this;
+		_.start = false;
 		_.clearTpl(document.querySelector('body'));
 		_.createPageTmp();
-		_.createField(size);
+		_.createFieldObject();
+		_.createField();
 	}
-	// создает разметку игровой страницы
-	createPageTmp(){
+	loadedGame(data){
 		const _ = this;
+		_.start = false;
+		_.clearTpl(document.querySelector('body'));
+		_.positions = data.positions;
+		let time = data.time;
+		let steps = data.steps;
+		_.createPageTmp(time,steps);
+		_.createField();
+	}
+
+
+	// создает разметку игровой страницы
+	createPageTmp(time = null,steps = null){
+		const _ = this;
+		let sec = time ? time[2] : '00',
+				min = time ? time[1] : '00',
+				hours = time ? time[0] : '00';
+
+		!steps ? steps = 0 : '';
+
 		let title = _.createEl('H1','main-title main-subtitle',{'text' : '"Пятнашки"'});
 		let row = _.createEl('DIV','row',{'children' : [
 				_.createEl('BUTTON','newGameBtn btn',{'text' : 'Начать заново'}),
 				_.createEl('DIV','steps',{'children' : [
 						_.createEl('SPAN','steps-title',{'text' : 'Ходы: '}),
-						_.createEl('SPAN','steps-span',{'text' : 0})
+						_.createEl('SPAN','steps-span',{'text' : steps})
 					]}),
 				_.createEl('DIV','time',{'children' : [
 						_.createEl('SPAN','time-title',{'text' : 'Время: '}),
 						_.createEl('DIV','time-row',{'children' : [
-								_.createEl('SPAN','time-hour',{'text' : '00'}),
+								_.createEl('SPAN','time-hour',{'text' : hours}),
 								_.createEl('SPAN','time-span',{'text' : ':'}),
-								_.createEl('SPAN','time-minute',{'text' : '00'}),
+								_.createEl('SPAN','time-minute',{'text' : min}),
 								_.createEl('SPAN','time-span',{'text' : ':'}),
-								_.createEl('SPAN','time-second',{'text' : '00'})
+								_.createEl('SPAN','time-second',{'text' : sec})
 							]})
 					]})
 			]});
-		row.querySelector('button').addEventListener('click',function () {
-			_.newGame(_.size);
-		});
 		let body = _.createEl('DIV','gameBody');
-		body.addEventListener('mousedown',function () {
-			if (!_.start) _.gameStarted();
-		});
 		let bottomRow = _.createEl('DIV','row',{'children' : [
 				_.createEl('BUTTON','loadBtn btn',{'text' : 'Загрузить'}),
 				_.createEl('BUTTON','saveBtn btn',{'text' : 'Сохранить'})
 			]});
-		bottomRow.querySelector('.loadBtn').addEventListener('click',function () {
-			_.finishScreenTpl();
-		});
 		document.querySelector('body').append(title,row,body,bottomRow);
+
+
+		row.querySelector('button').addEventListener('click',function () {_.newGame(_.size);});
+		body.addEventListener('mouseup',function () {
+			if (!_.start) {
+				_.start = true;
+				_.gameStarted()
+			}
+		});
+		bottomRow.querySelector('.loadBtn').addEventListener('click',function () {
+			_.loadGame();
+		});
+		bottomRow.querySelector('.saveBtn').addEventListener('click',function () {
+			_.saveGame();
+		});
 	}
-	// заполняет поле костями по порядку в количестве установленном игроком
-	createField(size = 4){
+	// создает объект расположение костяшек
+	createFieldObject(){
 		const _ = this;
-		if (typeof size !== 'number') return;
-		let int = (size * size) - 1,
-				body = document.querySelector('.gameBody');
+		let int = (_.size * _.size);
 		for (let i = 0; i < int; i++){
-			body.append(_.createEl('BUTTON',`bone pos${size}-${i + 1} pos${size}`,{'draggable':true,'children' : [
-					_.createEl('SPAN',null,{'text' : i + 1})
-				]}))
+			i < int - 1 ? _.positions[i + 1] = i + 1 : _.positions[i + 1] = null;
 		}
 	}
+	// заполняет поле костями по порядку в количестве установленном игроком
+	createField(){
+		const _ = this;
+		let body = document.querySelector('.gameBody');
+		for (let pos in _.positions){
+			if (_.positions[pos]){
+				let btn = _.createEl('BUTTON',`bone pos${_.size}`,{'id':`pos${_.size}-${pos}`,'draggable':true,'children' : [
+						_.createEl('SPAN',null,{'text' : _.positions[pos]})
+					]});
+				body.append(btn);
+
+				btn.addEventListener('click',function () {
+					_.possibleMoveCheck(btn)
+				})
+			}
+		}
+	}
+
 
 
 	// запускает методы отсчета времени и набора очков
 	gameStarted(){
 		const _ = this;
-		_.start = true;
-		setInterval(_.time,1000)
+		function startCheck () {
+			setTimeout(function () {
+				if (_.start) startCheck();
+				_.time();
+			},1000)
+		}
+		startCheck();
 	}
 	// показывает время во время игры))
 	time(){
-		let timeLabel = document.querySelector('.time-row');
-		let hours = timeLabel.querySelector('.time-hour');
-		let minutes = timeLabel.querySelector('.time-minute');
-		let seconds = timeLabel.querySelector('.time-second');
-		function lengthCheck(value){
-			value += '';
-			if (value.length < 2) value = '0' + value;
-			return value;
-		}
+		const _ = this;
+		if (document.querySelector('.time-row')){
+			let timeLabel = document.querySelector('.time-row');
+			let hours = timeLabel.querySelector('.time-hour');
+			let minutes = timeLabel.querySelector('.time-minute');
+			let seconds = timeLabel.querySelector('.time-second');
 
-		let sec = (seconds.textContent * 1) + 1,
-				min = minutes.textContent * 1,
-				h = hours.textContent * 1;
+			let sec = (seconds.textContent * 1) + 1,
+					min = minutes.textContent * 1,
+					h = hours.textContent * 1;
 
-		if (sec === 60) {
-			sec = 0;
-			min += 1;
-			if (min === 60){
-				min = 0;
-				h += 1;
-				hours.textContent = lengthCheck(h);
+			function lengthCheck(value){
+				value += '';
+				if (value.length < 2) value = '0' + value;
+				return value;
 			}
-			minutes.textContent = lengthCheck(min);
+
+			if (sec === 60) {
+				sec = 0;
+				min += 1;
+				if (min === 60){
+					min = 0;
+					h += 1;
+					if (_.start) hours.textContent = lengthCheck(h);
+				}
+				if (_.start) minutes.textContent = lengthCheck(min);
+			}
+			if (_.start) seconds.textContent = lengthCheck(sec);
 		}
-		seconds.textContent = lengthCheck(sec);
+	}
+
+
+	// методы игры
+	possibleMoveCheck(button){
+		const _ = this;
+		let id = button.id;
+		let pos = id.split('-')[1] * 1;
+		if (_.positions[pos + _.size] === null){
+			_.move(pos + _.size,button)
+		} else if (_.positions[pos + 1] === null){
+			_.move(pos + 1,button)
+		} else if (_.positions[pos - 1] === null){
+			_.move(pos - 1,button)
+		} else if (_.positions[pos - _.size] === null){
+			_.move(pos - _.size,button)
+		}
+	}
+	move(condition,button){
+		const _ = this;
+		let btnNumber = button.firstChild.textContent * 1;
+		let id = button.id;
+		let pos = id.split('-')[1] * 1;
+		_.positions[pos] = null;
+		_.positions[condition] = btnNumber;
+		button.id = `pos${_.size}-${condition}`;
+
+		let stepsCont = document.querySelector('.steps-span');
+		stepsCont.textContent = (stepsCont.textContent * 1) + 1;
+
+		_.checkToWin();
+	}
+	checkToWin(){
+		let gameBody = document.querySelector('.gameBody');
+		let check = true;
+		for (let i = 0; i < gameBody.children.length; i++){
+			if (gameBody.children[i].firstChild.textContent != gameBody.children[i].id.split('-')[1]) {
+				check = false;
+			}
+		}
+		if (check) {
+			let time = document.querySelector('.time-row').textContent,
+					steps = document.querySelector('.steps-span').textContent;
+			this.finishScreenTpl(time,steps);
+			this.start = false;
+		}
+	}
+
+
+	// сохранение игры
+	saveGame(){
+		const _ = this;
+		let time = document.querySelector('.time-row').textContent.split(':');
+		let steps = document.querySelector('.steps-span').textContent;
+		let saveData = {};
+		saveData.time = time;
+		saveData.steps = steps;
+		saveData.positions = _.positions;
+		saveData = JSON.stringify(saveData);
+		localStorage.setItem('15',saveData);
+	}
+	loadGame(){
+		const _ = this;
+		let loadData = localStorage.getItem('15');
+		loadData = JSON.parse(loadData);
+		_.loadedGame(loadData);
 	}
 
 
 	// создает разметку финальной страницы
-	finishScreenTpl(){
+	finishScreenTpl(time,steps){
 		const _ = this;
 		let body = document.querySelector('body');
 		_.clearTpl(body);
 		let title = _.createEl('H1','main-title finish-title',{'text' : 'Поздравляем! Вы победили!'});
 		let score = _.createEl('DIV','finish-score',{'children' : [
 			_.createEl('SPAN','finish-score-title',{'text' : 'Количество ходов: '}),
-			_.createEl('SPAN','finish-score-span',{'text' : '0'})
+			_.createEl('SPAN','finish-score-span',{'text' : steps})
 			]});
-		let time = _.createEl('DIV','finish-time',{'children' : [
+		let timeCont = _.createEl('DIV','finish-time',{'children' : [
 			_.createEl('SPAN','finish-time-title',{'text' : 'Затраченное время: '}),
-			_.createEl('SPAN','finish-time-span',{'text' : '0'})
+			_.createEl('SPAN','finish-time-span',{'text' : time})
 			]});
 		let newGameBtn = _.createEl('BUTTON','btn',{'text' : 'Начать новую игру'});
 		newGameBtn.addEventListener('click',function () {
 			_.init();
 		});
-		body.append(title,score,time,newGameBtn)
+		body.append(title,score,timeCont,newGameBtn)
 	}
 
 	init(){
