3178a69ed25b4cc3ac2126d38dd836c5f6740240
cekip88 <cekip@mail.ru>
2020-11-12 03:58:51 +0600

    init: реализованы методы:  1. Указание имени  2. Вывод таблицы счета  3. Перемешивание фишек
    fix: поправлены методы:
     1. Ход - устанен баг хода на 1 позицию на другой строке
     2. Загрузка игры при другом размере поля
     3. Новая игра при другом размере поля

--- main.js
+++ main.js
@@ -32,16 +32,32 @@ class Game {
 
 
 	// создает разметку первоначального экрана
+	nameCheck(){
+		if (localStorage.getItem('15-name')) {
+			this.name = JSON.parse(localStorage.getItem('15-name'));
+		}
+	}
 	firstScreenTpl(){
 		const _ = this;
 		let title = _.createEl('H1','main-title',{'text' : '"Пятнашки"'});
 		let sizeCount = _.createEl('DIV','choose',{'children' : [
-				_.createEl('SPAN','choose-title',{'text' : 'Размеры поля: '}),
-				_.createEl('INPUT','choose-input',{'text' : '4'})
+			_.createEl('SPAN','choose-title',{'text' : 'Размеры поля: '}),
+			_.createEl('INPUT','choose-input',{'text' : '4'})
+		]});
+		let check = _.createEl('DIV','checkbox',{'children' : [
+			_.createEl('SPAN','choose-title', {'text' : 'Перемешивание фишек: '}),
+			_.createEl('INPUT',null,{'type' : 'checkbox','id' : 'checkbox-input'}),
+			_.createEl('LABEL','checkbox-label',{'for' : 'checkbox-input'})
 			]});
+
+
+		let name = _.createEl('INPUT','name',{'type' : 'text','placeholder' : 'Введите Ваше имя'});
+		if (_.name) name.value = _.name;
+
+
 		let button = _.createEl('BUTTON','btn first-screen-btn',{'text' : 'Начать игру'});
 
-		document.querySelector('body').append(title,sizeCount,button);
+		document.querySelector('body').append(title,sizeCount,check,name,button);
 
 		sizeCount.querySelector('input').addEventListener('input',function () {
 			_.startSizeInputCheck(this)
@@ -50,7 +66,10 @@ class Game {
 			_.size = sizeCount.querySelector('input').value * 1;
 			if (_.size < 3) _.size = 3;
 			else if (_.size > 8) _.size = 8;
-			_.newGame()
+			_.name = document.querySelector('.name').value;
+			let rand = false;
+			if(document.querySelector('#checkbox-input').checked) rand = true;
+			_.newGame(rand)
 		});
 
 	}
@@ -60,11 +79,10 @@ class Game {
 
 
 
-
-
 	// запускает постройку игровой страницы
-	newGame(){
+	newGame(rand){
 		const _ = this;
+		localStorage.setItem('15-name',JSON.stringify(_.name));
 		_.start = false;
 		_.clearTpl(document.querySelector('body'));
 		let load = false;
@@ -72,15 +90,20 @@ class Game {
 		_.createPageTmp(null,null,load);
 		_.createFieldObject();
 		_.createField();
+		if (rand) _.randomMoves();
 	}
 	loadedGame(data){
 		const _ = this;
+		_.name = data.name;
 		_.start = false;
+		_.size = data.size;
 		_.clearTpl(document.querySelector('body'));
 		_.positions = data.positions;
 		let time = data.time;
 		let steps = data.steps;
-		_.createPageTmp(time,steps);
+		let load = false;
+		if (localStorage.getItem('15')) load = true;
+		_.createPageTmp(time,steps,load);
 		_.createField();
 	}
 
@@ -122,7 +145,7 @@ class Game {
 		document.querySelector('body').append(title,row,body,bottomRow);
 
 
-		row.querySelector('button').addEventListener('click',function () {_.newGame(_.size);});
+		document.querySelector('.newGameBtn').addEventListener('click',function () {_.init();});
 		body.addEventListener('mouseup',function () {
 			if (!_.start) {
 				_.start = true;
@@ -144,6 +167,7 @@ class Game {
 			i < int - 1 ? _.positions[i + 1] = i + 1 : _.positions[i + 1] = null;
 		}
 	}
+
 	// заполняет поле костями по порядку в количестве установленном игроком
 	createField(){
 		const _ = this;
@@ -156,11 +180,59 @@ class Game {
 				body.append(btn);
 
 				btn.addEventListener('click',function () {
-					_.possibleMoveCheck(btn)
+					_.buttonClick(btn);
+					_.stepsCount();
+					_.checkToWin();
 				})
 			}
 		}
 	}
+
+	// рандомно передвигает фишки
+	randomMoves(){
+		const _ = this;
+
+		let prevBtn = -500;
+		let interval = setInterval(function () {
+			let nl;
+			for (let pos in _.positions){
+				if (_.positions[pos] == null){
+					nl = pos * 1;
+				}
+			}
+
+			let countMoves = 0;
+			let btns = [];
+			if (_.positions[nl - _.size] && nl - _.size !== prevBtn) {
+				countMoves += 1;
+				btns.push(nl - _.size);
+			}
+			if (_.positions[nl + _.size] && nl + _.size !== prevBtn) {
+				countMoves += 1;
+				btns.push(nl + _.size);
+			}
+			if ((nl - 1) % _.size &&_.positions[nl - 1] && nl - 1 !== prevBtn) {
+				countMoves += 1;
+				btns.push(nl - 1);
+			}
+			if (nl % _.size && _.positions[nl + 1] && nl + 1 !== prevBtn) {
+				countMoves += 1;
+				btns.push(nl + 1);
+			}
+
+
+			let btnNumber = btns[Math.floor(Math.random() * countMoves)];
+			prevBtn = btnNumber;
+			_.move(nl,document.getElementById(`pos${_.size}-${btnNumber}`));
+
+			prevBtn = nl;
+		},100);
+		setTimeout(function () {
+			clearInterval(interval)
+		},_.size * 1000)
+//
+	}
+
 
 
 
@@ -210,15 +282,15 @@ class Game {
 
 
 	// методы игры
-	possibleMoveCheck(button){
+	buttonClick(button){
 		const _ = this;
 		let id = button.id;
 		let pos = id.split('-')[1] * 1;
 		if (_.positions[pos + _.size] === null){
 			_.move(pos + _.size,button)
-		} else if (_.positions[pos + 1] === null){
+		} else if (_.positions[pos + 1] === null && (pos % _.size) !== 0){
 			_.move(pos + 1,button)
-		} else if (_.positions[pos - 1] === null){
+		} else if (_.positions[pos - 1] === null && ((pos - 1) % _.size) !== 0){
 			_.move(pos - 1,button)
 		} else if (_.positions[pos - _.size] === null){
 			_.move(pos - _.size,button)
@@ -232,11 +304,10 @@ class Game {
 		_.positions[pos] = null;
 		_.positions[condition] = btnNumber;
 		button.id = `pos${_.size}-${condition}`;
-
+	}
+	stepsCount(){
 		let stepsCont = document.querySelector('.steps-span');
-		stepsCont.textContent = (stepsCont.textContent * 1) + 1;
-
-		_.checkToWin();
+		stepsCont.textContent = (stepsCont.textContent * 1) + 1 + '';
 	}
 	checkToWin(){
 		let gameBody = document.querySelector('.gameBody');
@@ -249,9 +320,45 @@ class Game {
 		if (check) {
 			let time = document.querySelector('.time-row').textContent,
 					steps = document.querySelector('.steps-span').textContent;
-			this.finishScreenTpl(time,steps);
+
 			this.start = false;
+			this.calcScore(time,steps);
 		}
+	}
+	calcScore(time,steps){
+		const _ = this;
+		time = time.split(':');
+		time = ((((time[0] * 1) * 60) + (time[1] * 1)) * 60) + (time[2] * 1);
+		steps = steps * 1;
+		let score = Math.ceil(((_.size * _.size * 1000) - time) / steps);
+
+		_.leadersListRefresh(score);
+		_.finishScreenTpl(time,steps,score);
+	}
+	leadersListRefresh(score){
+		const _ = this;
+		let leadersList = {};
+		if (localStorage.getItem('leadersList')) {
+			leadersList = JSON.parse(localStorage.getItem('leadersList'));
+		} else {
+			for (let i = 0; i < 10; i++){
+				leadersList[i + 1] = {
+					'score' : 0,
+					'name' : ' '
+				};
+			}
+		}
+
+		for (let pos in leadersList){
+			if (leadersList[pos]['score'] < score){
+				leadersList[pos]['score'] = score;
+				leadersList[pos]['name'] = _.name;
+				break;
+			}
+		}
+
+		leadersList = JSON.stringify(leadersList);
+		localStorage.setItem('leadersList',leadersList);
 	}
 
 
@@ -264,10 +371,13 @@ class Game {
 		saveData.time = time;
 		saveData.steps = steps;
 		saveData.positions = _.positions;
+		saveData.size = _.size;
+		saveData.name = _.name;
 		saveData = JSON.stringify(saveData);
 		localStorage.setItem('15',saveData);
 		if (document.querySelector('.loadBtn').classList.contains('inactive')) document.querySelector('.loadBtn').classList.remove('inactive');
 	}
+	// загрузка игры
 	loadGame(){
 		const _ = this;
 		let loadData = localStorage.getItem('15');
@@ -279,12 +389,12 @@ class Game {
 
 
 	// создает разметку финальной страницы
-	finishScreenTpl(time,steps){
+	finishScreenTpl(time,steps,score){
 		const _ = this;
 		let body = document.querySelector('body');
 		_.clearTpl(body);
 		let title = _.createEl('H1','main-title finish-title',{'text' : 'Поздравляем! Вы победили!'});
-		let score = _.createEl('DIV','finish-score',{'children' : [
+		let stepsCont = _.createEl('DIV','finish-steps',{'children' : [
 			_.createEl('SPAN','finish-score-title',{'text' : 'Количество ходов: '}),
 			_.createEl('SPAN','finish-score-span',{'text' : steps})
 			]});
@@ -292,17 +402,45 @@ class Game {
 			_.createEl('SPAN','finish-time-title',{'text' : 'Затраченное время: '}),
 			_.createEl('SPAN','finish-time-span',{'text' : time})
 			]});
+		let size = _.createEl('DIV','finish-size',{'children' : [
+			_.createEl('SPAN','finish-time-title',{'text' : 'Размеры поля: '}),
+			_.createEl('SPAN','finish-time-span',{'text' : _.size})
+			]});
+		let scoreCont = _.createEl('DIV','finish-score',{'children' : [
+			_.createEl('SPAN','finish-time-title',{'text' : 'Счет: '}),
+			_.createEl('SPAN','finish-time-span',{'text' : score})
+			]});
+
+		let scoreListTitle = _.createEl('DIV','finish-scorelist-title',{'children' : [
+				_.createEl('SPAN',null,{'text' : ''}),
+				_.createEl('SPAN',null,{'text' : 'Имя'}),
+				_.createEl('SPAN',null,{'text' : 'Счет'})
+			]});
+		let scoreList = _.createEl('UL','finish-scorelist');
+		let scoreListInfo = localStorage.getItem('leadersList');
+		scoreListInfo = JSON.parse(scoreListInfo);
+		for (let scr in scoreListInfo) {
+			let li = _.createEl('LI','finish-scoreitem',{'children' : [
+					_.createEl('SPAN',null,{'text' : `${scr}: `}),
+					_.createEl('SPAN',null,{'text' : scoreListInfo[scr]['name']}),
+					_.createEl('SPAN',null,{'text' : scoreListInfo[scr]['score']})
+				]});
+			scoreList.append(li)
+		}
+
 		let newGameBtn = _.createEl('BUTTON','btn',{'text' : 'Начать новую игру'});
 		newGameBtn.addEventListener('click',function () {
 			_.init();
 		});
-		body.append(title,score,timeCont,newGameBtn)
+		body.append(title,stepsCont,timeCont,size,scoreCont,scoreListTitle,scoreList,newGameBtn)
 	}
 
 	init(){
 		const _ = this;
+		_.nameCheck();
 		_.clearTpl(document.querySelector('body'));
 		_.firstScreenTpl();
+		_.positions = {};
 	}
 }
 
